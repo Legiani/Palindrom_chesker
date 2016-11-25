798872b315dd5c5bbf956420b148c9b7e82dd622
Legiani <kuba.bednar@seznam.cz>
2016-11-25 22:22:58 +0100

    Coments update

--- Palindromchesker/PalindromcheskerPage.xaml.cs
+++ Palindromchesker/PalindromcheskerPage.xaml.cs
@@ -5,18 +5,20 @@ namespace Palindromchesker
 {
 	public partial class PalindromcheskerPage : ContentPage
 	{
+		//inicalizuje xaml aplikace
 		public PalindromcheskerPage()
 		{
 			InitializeComponent();
 
-
 		}
+		//funkce se zeptá zda je řetezec pal......
 		public void odeslat(object sender, EventArgs e) {
 			vys.Text = jePalindrom(text.Text);
 		}
-
+		//vlastní kontrola
 		public static string jePalindrom(string retezec)
 		{
+			//vstupní řetezec je převeden na malá pismena a nasledně jsou nahrazeny české znaky a specialní znaky
 			retezec = retezec.ToLower()
 			                 .Replace(" ", "")
 			                 .Replace(".", "")
@@ -31,10 +33,13 @@ namespace Palindromchesker
 			                 .Replace("á", "a")
 			                 .Replace("í", "i")
 			                 .Replace("é", "e");
+			//řetezec je rozdělen do pole
 			char[] array = retezec.ToCharArray();
+			//pole je otočeno
 			Array.Reverse(array);
+			//z obraceného pole zpět na řetězec
 			string backwards = new string(array);
-
+			//porovnání a nasledé rozhodnutí je/není
 			if (retezec == backwards)
 			{
 				return "je";
